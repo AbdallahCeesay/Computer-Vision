090a47f9a1dc83c34f250cd91f6dc559139ef052
AbdallahCeesay <abdallah.ceesay@students.plymouth.ac.uk>
2025-02-13 16:48:47 +0000

    Identifying more colours
    Now identifying red, green, blue and yellow with the HSV conversion

--- ROCO321Part1/Task 1/main.cpp
+++ ROCO321Part1/Task 1/main.cpp
@@ -1,4 +1,4 @@
-//James Rogers Oct 2023 (c) Plymouth University
+﻿//James Rogers Oct 2023 (c) Plymouth University
 
 #include <iostream>
 #include <fstream>
@@ -24,7 +24,7 @@ int main()
 
         //get pixel colour values
         Point centrePoint(left.size().width/2, left.size().height/2); // sets the size of the centrepoint
-        Vec3b pixelValue = left.at<Vec3b>(centrePoint); //gets the pixel values from the centrepoint of the image stored in left
+        Vec3b pixelValue = left.at<Vec3b>(centrePoint); //gets the pixel values from the centrepoint of the image stored in left cam
         unsigned char red  =pixelValue[2];
         unsigned char green=pixelValue[1];
         unsigned char blue =pixelValue[0];
@@ -32,7 +32,7 @@ int main()
         /*note that in openCV RGB is reversed to BGR*/
 
         /*conversion from RGB - HSV*/ 
-        cvtColor(left, hsv_image, COLOR_RGB2HSV);
+        cvtColor(left, hsv_image, COLOR_BGR2HSV);
 
         /*defining colour thresholds*/
         Scalar lower_red (0,100,100), upper_red (20,200,200);
@@ -42,22 +42,46 @@ int main()
         inRange(hsv_image, lower_red, upper_red, mask_red);
 
         /*gets the hsv colour value at the centre of the pixel hsv_image */
-        Vec3b hsv_image_pixel = left.at<Vec3b> (centrePoint);
+        Vec3b hsv_image_pixel = hsv_image.at<Vec3b> (centrePoint);
         int hue = hsv_image_pixel[0];
-        int sat = hsv_iamge_pixel[1];
+        int sat = hsv_image_pixel[1];
         int val = hsv_image_pixel[2];
 
         string textColour;
-        
-        if (hue >= 0 && hue <= 20) {
-            if (sat >= 100 && sat <= 200) {
-                if (val >= 100 && val <= 200) {
-
-                    textColour = "red";
-                    putText(left, textColour, centrePoint+Point(-50,100), FONT_HERSHEY_SIMPLEX, 1, Scalar(255,0,0), 2);
-                }
+        /*RED*/
+        if ((hue >= 170 && hue <= 179) || (hue >= 1 && hue <= 9)) {
+            if (sat >=125 && sat <= 250) {
+                textColour = "RED";
+                putText(left, textColour, centrePoint+Point(200,200), FONT_HERSHEY_SIMPLEX, 1, Scalar(0,0,255), 2);
             }
         }
+
+        /*GREEN*/
+        if (hue >= 50 && hue <= 65) {
+            if (sat >= 125 && sat <=250) {
+                textColour = "GREEN";
+                putText(left, textColour, centrePoint+Point(200,200), FONT_HERSHEY_SIMPLEX, 1, Scalar(0,255,0), 2);
+            }
+        }
+
+        /*BLUE*/
+        if (hue >= 105 && hue <= 125) {
+            if (sat >= 125 && sat <= 250) {
+                textColour = "BLUE";
+                putText(left, textColour, centrePoint+Point(200,200), FONT_HERSHEY_SIMPLEX, 1, Scalar(255,0,0), 2);
+            }
+        }
+
+        /*YELLOW*/
+        if (hue >= 25 && hue <= 35) {
+            if (sat >= 125 && sat <= 250) {
+                textColour = "YELLOW";
+                putText(left, textColour, centrePoint+Point(200,200), FONT_HERSHEY_SIMPLEX, 1, Scalar(0,0,255), 2);
+            }
+        }
+
+
+        cout << val << endl;
 
         /*drawing functions*/
         circle(left, centrePoint, 15, Scalar(255,255,255), 2); //draw a circle to show the pixel being measured
